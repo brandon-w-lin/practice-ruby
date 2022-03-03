cf41450176afda6f1bed831efe16acbb1a31e920
Brandon Lin <bwlin93@gmail.com>
2022-03-03 11:42:38 -0600

    Reconfigures to use modules to dry the code

--- module_example.rb
+++ module_example.rb
@@ -1,20 +1,31 @@
-class Car
+module Brake 
+  def brake
+    @speed = 0
+  end
+end
+
+module Accelerate
+  def accelerate
+    @speed += 10
+  end 
+end
+
+module Turn 
+  def turn(new_direction)
+    @direction = new_direction
+  end
+end
+
+module InitVehicle
+  attr_accessor :brake, :speed, :turn, :direction
   def initialize
     @speed = 0
     @direction = 'north'
   end
+end
 
-  def brake
-    @speed = 0
-  end
-
-  def accelerate
-    @speed += 10
-  end
-
-  def turn(new_direction)
-    @direction = new_direction
-  end
+class Car
+  include InitVehicle, Brake, Accelerate, Turn
 
   def honk_horn
     puts "Beeeeeeep!"
@@ -22,24 +33,22 @@ class Car
 end
 
 class Bike
-  def initialize
-    @speed = 0
-    @direction = 'north'
-  end
-
-  def brake
-    @speed = 0
-  end
-
-  def accelerate
-    @speed += 10
-  end
-
-  def turn(new_direction)
-    @direction = new_direction
-  end
+  include InitVehicle, Brake, Accelerate, Turn
 
   def ring_bell
     puts "Ring ring!"
   end
 end
+
+
+bike = Bike.new()
+bike.ring_bell
+puts bike.speed
+
+car = Car.new()
+car.honk_horn
+puts car.direction
+
+puts car.speed
+car.accelerate
+puts car.speed
