e0ae79806bd3bcc808dafad6d1f96a0b7dee9ff7
Brandon Lin <bwlin93@gmail.com>
2022-03-08 08:33:43 -0600

    adds untracked changes

---
+++ bowling.rb
@@ -0,0 +1,45 @@
+# Keep track of a score for a single roll
+# Keep track of how many pins are left after the roll
+# Add condition to determine if second roll is needed
+# Keep track of the score for each of the first two rolls
+# Update the scoring for a single frame to allow for strikes
+# Update the scoring for a single frame to allow for spares
+# Allow for multiple frames to be played
+# Build condition to allow for 3 rolls in frame 10 if strike is scored on first roll
+# Return the total score
+
+class Bowl
+
+  def roll
+    puts "pins knocked down on first roll"
+    @pins1 = gets.chomp
+    if not @pins = 10
+      puts "pins knocked down on second roll"
+      @pins2 = gets.chomp
+    end
+  end
+
+  def score()
+    if @pins1 = 10 #strike
+      return 25 #whatever strike score is
+    elsif @pins1 + @pins2 = 10 #spare
+      return 15 #whatever spare is
+    else
+      return @pins1 + @pins2
+    end
+  end
+
+  def play
+    i = 0
+    # 2.times do
+      roll
+      pins = []
+      pins[i][0] = @pins1
+      pins[i][1] = @pins2
+      scores[i] = score
+      i+=1
+      scores.each {|score| puts "scores were: #{score}"}
+    # end
+  end
+
+end
---
+++ pairpractice.rb
@@ -0,0 +1,21 @@
+# count = 0
+
+# 11.times do
+#   p count
+#   count +=1
+# end
+
+# recipes = ['crepes','b','c','d','e']
+# languages = ['french','y','x']
+
+# if recipes.length() > 10 && languages.length() > 5
+#   puts 'you should date!!!!!!!!'
+# else
+#   puts 'you should not date :('
+# end
+
+# if recipes.include? 'crepes' or languages.include? 'french'
+#   puts 'you should marry!!!!'
+# else 
+#   puts 'you should not get married'
+# end
