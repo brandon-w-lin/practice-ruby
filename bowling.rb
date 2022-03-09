ad562c771fe32c29c573cbc4f6bdf801cd0db193
Brandon Lin <bwlin93@gmail.com>
2022-03-08 19:20:53 -0600

    adds functionality to convert an array of scores for displaying on scoreboard

--- bowling.rb
+++ bowling.rb
@@ -45,7 +45,49 @@ class Bowl
     end
     @scores.each_with_index { |score, index| puts "score in set #{index + 1} was: #{score}" }
   end
+
+  def conv_scores_to_scoreboard(scores)
+    scores_for_scoreboard = []
+    scores.each do |score|
+      if score == 25
+        scores_for_scoreboard << "  X  |"
+      elsif score == 15
+        scores_for_scoreboard << "  /  |"
+      else
+        if score >= 10
+          scores_for_scoreboard << " #{score}  |"
+        else
+          scores_for_scoreboard << "  #{score}  |"
+        end
+      end
+    end
+
+    blank_score = "     |"
+    (10 - scores.length).times do
+      scores_for_scoreboard << blank_score
+    end
+
+    scores_string = ""
+    scores_for_scoreboard.each do |score|
+      scores_string += score
+    end
+    return scores_string
+  end
+
+  def scoreboard()
+    line_star = "**************************************************************************"
+    line_underscore = "__________________________________________________________________________"
+    header = "  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  | 10  | Total Score |"
+    spacing_count = 15
+    spacing = " " * spacing_count + "|"
+
+    puts spacing + line_star
+    puts spacing + header
+    puts spacing + line_star
+    puts "Player 1" + (" " * (spacing_count - "Player 1".length)) + "|" + scores_for_player(player1)
+    puts spacing + line_underscore
+  end
 end
 
 game = Bowl.new
-game.play
+game.conv_scores_to_scoreboard([8, 10, 15, 25])
